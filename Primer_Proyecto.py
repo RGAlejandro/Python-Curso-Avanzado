44b70c3e7999675ac92bdf16233aea69a2c1d6ba
RGAlejandro <133199401+RGAlejandro@users.noreply.github.com>
2024-01-08 13:36:36 +0100

    Add files via upload

---
+++ Primer_Proyecto.py
@@ -0,0 +1,4 @@
+print ("MARCA DE CERVEZA")
+
+print(input("Introduce la primera respuesta: ")+" "+input("Introduce la segunda respuesta: "))
+print("La cerveza se llama \n")
---
+++ cerveza.py
@@ -0,0 +1,1 @@
+print("El nombre de tu cerveza es\n"+input("¿Que ciudad te gustaria visitar?") +input("¿Cual es tu color favorito?")+" \nFelicitaciones")
